c0f8b6ca700fcf96e285d9b0f1dc7d21b5da1ad8
agasurfer <agasurfer@hotmail.com>
2025-03-10 16:34:28 +0100

    click scope fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,28 +11,28 @@ const [activeTab, setActiveTab] = useState("overview");
     return (
     <nav>
         
-        <ul className="tabs">
-            <li className={`tab ${activeTab === "overview" ? "active" : ""}`}
-            onClick={() => setActiveTab("overview")}>
-                <Link to='/' >🌍 Overview</Link>
-            </li>
-            <li className={`tab ${activeTab === "footprint" ? "active" : ""}`}
-            onClick={() => setActiveTab("footprint")}>
-                <Link to='/footprint' >👣 Footprint</Link>
-            </li>
-            <li className={`tab ${activeTab === "containers" ? "active" : ""}`}
-            onClick={() => setActiveTab("containers")}>
-                <Link to='/containers' >🧊 Containers</Link>
-            </li>
-            <li className={`tab ${activeTab === "buckets" ? "active" : ""}`}
-            onClick={() => setActiveTab("buckets")}>
-                <Link to='/buckets' ><img src={bucket} alt="" className="bucket-img" />Buckets</Link>
-            </li>
-            <li className={`tab ${activeTab === "volumes" ? "active" : ""}`}
+        <div className="tabs">
+            <Link to='/' className={`tab ${activeTab === "overview" ? "active" : ""}`}
+            onClick={() => setActiveTab("overview")} >
+                🌍 Overview
+            </Link>
+            <Link to='/footprint' className={`tab ${activeTab === "footprint" ? "active" : ""}`}
+            onClick={() => setActiveTab("footprint")} >
+                👣 Footprint
+            </Link>
+            <Link to='/containers' className={`tab ${activeTab === "containers" ? "active" : ""}`}
+            onClick={() => setActiveTab("containers")} >
+                🧊 Containers
+            </Link>
+            <Link to='/buckets' className={`tab ${activeTab === "buckets" ? "active" : ""}`}
+            onClick={() => setActiveTab("buckets")} >
+                <img src={bucket} alt="" className="bucket-img" />Buckets
+            </Link>
+            <Link to='/volumes' className={`tab ${activeTab === "volumes" ? "active" : ""}`}
             onClick={() => setActiveTab("volumes")}>
-                <Link to='/volumes' ><img src={bloc} alt="" className="bloc-img" />Volumes</Link>
-            </li>
-        </ul>
+                <img src={bloc} alt="" className="bloc-img" />Volumes
+            </Link>
+        </div>
     </nav>
   )
 }
